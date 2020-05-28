89055dbfc5116571c1e1978f55fcc8082513c63e
Theobaldtian <62706607+Theobaldtian@users.noreply.github.com>
2020-05-28 19:35:27 +0800

    Create string.h

---
+++ Lesson_OOP_P21/string.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef STRING_H
+#define STRING_H
+
+class String {
+public:
+	String(char const *chars = "");
+	String(String const& str);
+	~String();
+
+	void display() const;
+
+private:
+	char* ptrChars;
+
+};
+
+#endif
